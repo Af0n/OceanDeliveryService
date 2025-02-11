fdde784f3d6cb08c19b17dcdea0c1c159682b287
AndAgGit <afon.cyffrous@gmail.com>
2025-02-10 17:04:23 -0800

    ATM.OS Terminal basic functionality done
    Wind direction determined through windCodes. For some reason the OnClick editor got some serious downgrades and it has limited functionality when it comes to input.
    
    Working around OnClick limitations
        0 = North
        1 = North-East
        2 = East
        3 = South-East
        4 = South
        5 = South-West
        6 = West
        7 = North-West

--- Assets/Scripts/ATMOS_Terminal.cs
+++ Assets/Scripts/ATMOS_Terminal.cs
@@ -1,24 +1,42 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 public class ATMOS_Terminal : MonoBehaviour
 {
-    [Header("Position camera changes to when switching direction.")]
+    public float speed;
+    [Header("Unity Set Up")]
+    [Tooltip("Position camera changes to when switching direction.")]
     public Transform CamPos;
+    [Tooltip("UI for terminal")]
     public Transform TerminalUI;
+
+    private WindManager windManager;
+
+    private void Start() {
+        windManager = WindManager.Instance;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Interact(Camera.main.transform);
+        }
+    }
 
     public void Interact(Transform cam){
         // [TODO] lock player movement
-        cam.position = CamPos.position; // [TODO] replace with a nicer transition
-
+        cam.SetPositionAndRotation(CamPos.position, CamPos.rotation); // [TODO] smoother transition
         Cursor.lockState = CursorLockMode.None; // Unlock cursor
         TerminalUI.gameObject.SetActive(true); // show UI
     }
 
     public void Cancel(Transform cam){
-        cam.localPosition = new Vector3(0f,0.75f,0f); // [TODO] replace with a nicer transition and non-hardcoded values
-        
+        // [TODO] unlock player movement
+        cam.SetLocalPositionAndRotation(new Vector3(0f,0.75f,0f), Quaternion.identity); // [TODO] smoother transition
         Cursor.lockState = CursorLockMode.Locked; // lock cursor
         TerminalUI.gameObject.SetActive(false); // hide UI
+    }
+
+    // i had to do it this way for it to be recognized in the unity event for buttons
+    public void SetWind(int windCode){
+        windManager.SetWind(windCode);
     }
 }
--- Assets/Scripts/WindManager.cs
+++ Assets/Scripts/WindManager.cs
@@ -1,24 +1,35 @@
-using NUnit.Framework.Internal;
 using UnityEngine;
 
 public class WindManager : MonoBehaviour
 {
+    /* Working around OnClick limitations
+    0 = North
+    1 = North-East
+    2 = East
+    3 = South-East
+    4 = South
+    5 = South-West
+    6 = West
+    7 = North-West
+    */
+
     public Transform WindVisual;
     [Header("Testing")]
     public bool Test;
     public Vector3 TestWind;
 
-    private Vector3 windDir;
-    private static WindManager instance;
+    [HideInInspector]
+    public static WindManager Instance;
 
+    private Vector3 windDir;
+    
     private void Awake() {
         windDir = new Vector3(0,0,0);
 
         // singleton
-        if(instance != null){
-            return;
+        if(Instance == null){
+            Instance = this;
         }
-        instance = this;
     }
 
     private void Update() {
@@ -27,15 +38,43 @@ public class WindManager : MonoBehaviour
         }
     }
 
-    public static WindManager GetInstance(){
-        return instance;
-    }
-
     public void SetWind(Vector3 wind){
         windDir = wind;
+
+        var vOverL = WindVisual.GetComponent<ParticleSystem>().velocityOverLifetime;
+        vOverL.x = windDir.x;
+        vOverL.z = windDir.z;
     }
 
     public void SetWind(Vector3 direction, float speed){
         SetWind(direction * speed);
+    }
+
+    public void SetWind(int windCode){
+        Vector3 direction = WindCodetoVec3(windCode);
+        
+        SetWind(direction);
+    }
+
+    public Vector3 WindCodetoVec3(int windCode){
+        switch(windCode){
+            case 0:
+                return new Vector3(0f, 0f, 10f);
+            case 1:
+                return new Vector3(7.071f, 0f, 7.071f);
+            case 2:
+                return new Vector3(10f, 0f, 0f);;
+            case 3:
+                return new Vector3(7.071f, 0f, -7.071f);;
+            case 4:
+                return new Vector3(0f, 0f, -10f);;
+            case 5:
+                return new Vector3(-7.071f, 0f, -7.071f);;
+            case 6:
+                return new Vector3(-10f, 0f, 0f);
+            case 7:
+                return new Vector3(-7.071f, 0f, 7.071f);
+        }
+        return Vector3.zero;
     }
 }
