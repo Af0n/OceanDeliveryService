bcf5265cebec8ff5e3574bb74a723af9d2fd2a79
AndAgGit <afon.cyffrous@gmail.com>
2025-02-14 14:07:53 -0800

    Start work on pause script

---
+++ Assets/Scripts/Pause_UI/PauseUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    public Transform MapPanel;
+    public Transform DirPanel;
+    public Transform InvPanel;
+    public Transform InfPanel;
+    public Transform SysPanel;
+
+    
+}
