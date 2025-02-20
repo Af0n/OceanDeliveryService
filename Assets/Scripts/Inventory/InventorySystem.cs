e05c8285770b320e7ebca205b8140a5cfc02004b
mbelles21 <mbelles@csumb.edu>
2025-02-20 12:26:22 -0800

    added inventory autofill
    automatically puts objects in next open space if there's any left

--- Assets/Scripts/Inventory/InventorySystem.cs
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -3,7 +3,7 @@ using CodeMonkey.Utils;
 using System.Collections.Generic;
 using System;
 
-public class GridBuildingSystem : MonoBehaviour
+public class InventorySystem : MonoBehaviour
 {
     public List<InventoryObject> inventoryObjects;
     private InventoryObject inventoryObject;
@@ -12,10 +12,13 @@ public class GridBuildingSystem : MonoBehaviour
     private Grid<GridObject> grid;
     private InventoryObject.Dir dir = InventoryObject.Dir.Down;
 
+    public int width;
+    public int height;
+
     void Awake()
     {
-        int gridWidth = 10;
-        int gridHeight = 10;
+        int gridWidth = width;
+        int gridHeight = height;
         float cellSize = 10f;
         grid = new Grid<GridObject>(gridWidth, gridHeight, cellSize, Vector3.zero, (Grid<GridObject> g, int x, int y) => new GridObject(g, x,  y));
 
@@ -27,6 +30,7 @@ public class GridBuildingSystem : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0)) {
             Vector3 pos = grid.GetNearestGridSegment(Mouse3D.GetMouseWorldPosition());
+            Debug.Log(pos);
             // TODO: clicking outside the grid throws an error -- fix
 
             // need to divide by grid cell size to avoid error (note: this is bc grid starts at 0,0)
@@ -82,11 +86,54 @@ public class GridBuildingSystem : MonoBehaviour
         }
 
         if(Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("switched box");
             index++;
             if(index >= inventoryObjects.Count) {
                 index = 0;
             }
             inventoryObject = inventoryObjects[index];
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            AddObjectToInventory(inventoryObject);
+        }
+    }
+
+    private void AddObjectToInventory(InventoryObject inventoryObject) 
+    {
+        bool objectAdded = false;
+        if(objectAdded) return; // so that only one object gets added per call
+
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                List<Vector2Int> gridPosList = inventoryObject.GetGridPositionList(new Vector2Int(x, y), dir);
+                bool canPlace = true;
+                foreach(Vector2Int gridPos in gridPosList) {
+                    if(gridPos.x < 0 || gridPos.y < 0 || gridPos.x >= width || gridPos.y >= height) {
+                        canPlace = false; // out of bounds check
+                        break;
+                    }
+
+                    if(!grid.GetGridObject(gridPos.x, gridPos.y).CanBuild()) {
+                        canPlace = false;
+                        break;
+                    }
+                }
+
+                if(canPlace) {
+                    Vector2Int rotationOffset = inventoryObject.GetRotationOffset(dir);
+                    Vector3 placedPos = new Vector3(x*10, y*10, 0) + new Vector3(rotationOffset.x, rotationOffset.y, 0) * grid.GetCellSize();
+
+                    PlacedObject placedObject = PlacedObject.Create(placedPos, new Vector2Int(x, y), dir, inventoryObject);
+                    
+                    foreach(Vector2Int gridPos in gridPosList) {
+                        grid.GetGridObject(gridPos.x, gridPos.y).SetPlacedObject(placedObject);
+                    }
+
+                    objectAdded = true;
+                    return;
+                }
+            }
         }
     }
 
