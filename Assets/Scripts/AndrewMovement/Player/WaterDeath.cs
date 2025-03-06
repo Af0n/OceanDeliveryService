d0a22316bb047535f8174ee7b60b5876f4a5ee47
AndAgGit <afon.cyffrous@gmail.com>
2025-03-05 17:35:44 -0800

    Added a visual for drowning

--- Assets/Scripts/AndrewMovement/Player/WaterDeath.cs
+++ Assets/Scripts/AndrewMovement/Player/WaterDeath.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterDeath : MonoBehaviour
 {
+    [Tooltip("How long it takes for our robot to drown")]
+    public float DrownTime;
+    [Header("Unity Set Up")]
+    public Image WaterVisual;
+
     private PlayerManager playerMan;
     private bool isDrowning;
 
-    [Tooltip("How long it takes for our robot to drown")]
-    public float DrownTime;
+    private float drownTimer;
+    public float PercentDrowned{
+        get{ return drownTimer / DrownTime; }
+    }
 
     private void Awake() {
         playerMan = GetComponent<PlayerManager>();
         isDrowning = false;
+        drownTimer = 0;
     }
 
     public void StartDrowning(){
@@ -26,11 +35,18 @@ public class WaterDeath : MonoBehaviour
 
     public void StopDrowning(){
         isDrowning = false;
+        drownTimer = 0;
+        WaterVisual.fillAmount = PercentDrowned;
         StopAllCoroutines();
     }
 
     public IEnumerator DrownTimer(){
-        yield return new WaitForSeconds(DrownTime);
+        while(PercentDrowned<1){
+            drownTimer += Time.deltaTime;
+            WaterVisual.fillAmount = PercentDrowned;
+            // waits for next frame
+            yield return 0;
+        }
         playerMan.Die("drowning");
         StopDrowning();
     }
