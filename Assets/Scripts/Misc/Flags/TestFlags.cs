10c55c02246b539f8df3304e4f0c78f6e5fe16b2
AndAgGit <afon.cyffrous@gmail.com>
2025-03-07 13:42:39 -0800

    Further testing
    Works poggly

--- Assets/Scripts/Misc/Flags/TestFlags.cs
+++ Assets/Scripts/Misc/Flags/TestFlags.cs
@@ -6,15 +6,27 @@ public class TestFlags : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.T)){
+            if(Input.GetKey(KeyCode.LeftShift)){
+                list.SetFlag("OtherTestFlag", true);
+                return;
+            }
             list.SetFlag("TestFlag", true);
         }
 
         if(Input.GetKeyDown(KeyCode.F)){
+            if(Input.GetKey(KeyCode.LeftShift)){
+                list.SetFlag("OtherTestFlag", false);
+            }
             list.SetFlag("TestFlag", false);
         }
 
         if(Input.GetKeyDown(KeyCode.B)){
+            if(Input.GetKey(KeyCode.LeftShift)){
+                list.SetFlag("OtherTestFlag");
+            }
             list.SetFlag("TestFlag");
         }
+
+        
     }
 }
