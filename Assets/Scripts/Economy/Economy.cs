89052b306df476bf1a6190c955deb8c55c562cc4
AndAgGit <afon.cyffrous@gmail.com>
2025-03-12 17:20:18 -0700

    Created prefabs for each of the scraps

---
+++ Assets/Scripts/Economy/Economy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public static class Economy
+{
+    public static int Scrap;
+
+    public static void ChangeScrap(int amount){
+        Scrap += amount;
+        Scrap = Mathf.Clamp(Scrap, 0, 999);
+        Debug.Log("Scrap: " + Scrap);
+    }
+}
