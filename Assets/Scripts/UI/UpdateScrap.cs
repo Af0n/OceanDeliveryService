c9defe2508359f1f0fd066ff71c861c0564b01ed
AndAgGit <afon.cyffrous@gmail.com>
2025-03-14 11:32:25 -0700

    Fixing Pause UI in Player Prefab
    Also added a scrap display

---
+++ Assets/Scripts/UI/UpdateScrap.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class UpdateScrap : MonoBehaviour
+{
+    [Tooltip("How long to wait between updates")]
+    public float UpdateDelay;
+
+    private TextMeshProUGUI textBox;
+
+    private void Start() {
+        textBox = GetComponent<TextMeshProUGUI>();
+    }
+
+    private IEnumerator UpdateLoop(){
+        yield return new WaitForSeconds(UpdateDelay);
+
+        textBox.text = Economy.Scrap.ToString();
+    }
+}
